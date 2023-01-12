cfe640088afea4b35273152153ce5e268c6e0e34
frankD <fdimicco@gmail.com>
2023-01-12 13:44:19 -0500

    raycast won't hit player now

--- Assets/Scripts/Player/GrappleRayCast.cs
+++ Assets/Scripts/Player/GrappleRayCast.cs
@@ -5,23 +5,28 @@ using UnityEngine.InputSystem;
 
 public class GrappleRayCast : MonoBehaviour
 {
-    //This script is a component to RayPoint, a child of Player
+    //*******************This script is a component to RayPoint, a child of Player*******************************
 
-    float drawRayTime = 1.5f;
+    public float rayCastLength = 100f;
 
     public RaycastHit2D ray;
-    Vector3 mousePosition; //position of mouse
+    Vector3 mousePosition; //position of the mouse
     Vector3 worldPosition; //mousePosition converted to worldPosition format
+    LayerMask groundLayer; //Used in Raycast to only register hits with the ground layer
+
+
+    private void Start() {
+        groundLayer = LayerMask.GetMask("Ground");
+    }
+
 
     void Update()
     {
-
         mousePosition = Mouse.current.position.ReadValue(); // gets mouse cursor coordinates
         worldPosition = Camera.main.ScreenToWorldPoint(mousePosition); // converts mouse coordinates to world point coordinates vector3
-        ray = Physics2D.Raycast(transform.position, worldPosition - transform.position, 10f); //cast ray in direction of mouse
+        ray = Physics2D.Raycast(transform.position, worldPosition - transform.position, rayCastLength, groundLayer); //cast ray in direction of mouse
 
-       // Debug.DrawRay(transform.position, worldPosition - transform.position, Color.green, drawRayTime);
+       // Debug.DrawRay(transform.position, worldPosition - transform.position, Color.green, 1.5f);
        
-
     }
 }
